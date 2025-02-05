faecacc9477089a570e7c623648cf2690111cffd
amarmontrout <numbest_rougher0y@icloud.com>
2025-02-05 14:27:58 -0700

    Complete Week 2 Assignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,8 +29,8 @@ const Home = () => {
   };
 
   const handleSaveEdit = (item) => {
-    setInputList((prev) =>
-      prev.map((entry) => (entry === item ? editText : entry))
+    setInputList((prevList) =>
+      prevList.map((entry) => (entry === item ? editText : entry))
     );
     setEditingItem(null);
   };
@@ -46,56 +46,63 @@ const Home = () => {
           handleSubmit={handleSubmit}
           inputText={inputText}
         />
-        {inputList.map((item) => {
-          return (
-            <div key={item} className="list-item">
-              <div>
-                {editingItem === item ? (
-                  <input
-                    value={editText}
-                    onChange={(e) => setEditText(e.target.value)}
-                    onBlur={() => handleSaveEdit(item)}
-                    autoFocus
-                  />
-                ) : (
-                  item
-                )}
+        {inputList.length === 0 ? (
+          <h3>No movies found. Please add a movie to your list.</h3>
+        ) : (
+          inputList.map((item, index) => {
+            return (
+              <div key={index} className="list-item">
+                <div className="movie-title">
+                  {editingItem === item ? (
+                    <input
+                      id="edit-input"
+                      value={editText}
+                      onChange={(e) => setEditText(e.target.value)}
+                      onBlur={() => handleSaveEdit(item)}
+                      autoFocus
+                    />
+                  ) : (
+                    item
+                  )}
+                </div>
+                <div className="btns">
+                  <button
+                    onClick={() => {
+                      handleEdit(item);
+                    }}
+                    className="edit-btn"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24px"
+                      viewBox="0 -960 960 960"
+                      width="24px"
+                      fill="#000"
+                    >
+                      <path d="M160-400v-80h280v80H160Zm0-160v-80h440v80H160Zm0-160v-80h440v80H160Zm360 560v-123l221-220q9-9 20-13t22-4q12 0 23 4.5t20 13.5l37 37q8 9 12.5 20t4.5 22q0 11-4 22.5T863-380L643-160H520Zm300-263-37-37 37 37ZM580-220h38l121-122-18-19-19-18-122 121v38Zm141-141-19-18 37 37-18-19Z" />
+                    </svg>
+                  </button>
+                  <button
+                    onClick={() => {
+                      handleDelete(item);
+                    }}
+                    className="delete-btn"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24px"
+                      viewBox="0 -960 960 960"
+                      width="24px"
+                      fill="#000"
+                    >
+                      <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                    </svg>
+                  </button>
+                </div>
               </div>
-              <button
-                onClick={() => {
-                  handleEdit(item);
-                }}
-                className="edit-btn"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                  fill="#000"
-                >
-                  <path d="M160-400v-80h280v80H160Zm0-160v-80h440v80H160Zm0-160v-80h440v80H160Zm360 560v-123l221-220q9-9 20-13t22-4q12 0 23 4.5t20 13.5l37 37q8 9 12.5 20t4.5 22q0 11-4 22.5T863-380L643-160H520Zm300-263-37-37 37 37ZM580-220h38l121-122-18-19-19-18-122 121v38Zm141-141-19-18 37 37-18-19Z" />
-                </svg>
-              </button>
-              <button
-                onClick={() => {
-                  handleDelete(item);
-                }}
-                className="delete-btn"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                  fill="#000"
-                >
-                  <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-                </svg>
-              </button>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </main>
       <Footer />
     </div>
